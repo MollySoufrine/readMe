ab7eb04d4a1fb36bc8c83271afca11f4896d70f3
molly soufrine <mollysoufrine@gmail.com>
2020-07-19 19:45:33 -0400

    added in functions and objects

--- index.js
+++ index.js
@@ -1,54 +1,73 @@
 //Loading required Modules
 //install npm package inquierer
 const inquirer = require("inquirer");
-const generateMarkdown = require("./utils/generateMarkdown");
+const fs = require("fs");
+const generateMarkdown = require("./generateMarkdown");
 
 // array of questions for user
-function promptUser() {
-  return inquirer.prompt([
-    {
-      type: "input",
-      name: "github",
-      message: "What is your GitHub username?",
-    },
-    {
-      type: "input",
-      name: "name",
-      message: "What is your name?",
-    },
-    {
-      type: "input",
-      name: "email",
-      message: "What is your email?",
-    },
-    {
-      type: "input",
-      name: "project",
-      message: "Which project is this?",
-    },
-    {
-      type: "input",
-      name: "linkedin",
-      message: "What is your linkedin username?",
-    },
-    {
-      type: "input",
-      name: "describe",
-      message: "Can you please give a breif description of this project?",
-    },
 
-    //rest of the questions here....
-  ]);
-}
+const questions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is your name?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is your email?",
+  },
+  {
+    type: "input",
+    name: "project",
+    message: "Which project is this?",
+  },
+  {
+    type: "input",
+    name: "describe",
+    message: "Can you please give a breif description of this project?",
+  },
+  {
+    type: "input",
+    name: "license",
+    message: "What is your current license?",
+  },
+  {
+    type: "input",
+    name: "information",
+    message: "What is the use of this project?",
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "How would a user install this?",
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What test instructions should be followed?",
+  },
+
+  //rest of the questions here....
+];
 
 // function to write README file
-promptUser().then(function (data) {
-  console.log(data);
-  writeFileAsync("readme.md", md);
-});
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(fileName, data);
+}
 
 // function to initialize program
-function init() {}
+function init() {
+  inquirer.prompt(questions).then(function (data) {
+    console.log(data);
+    writeToFile("README.md", generateMarkdown(data));
+  });
+}
 
 // function call to initialize program
 init();
